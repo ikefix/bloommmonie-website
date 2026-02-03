71a0496b25f508c5d8431a076fe717f65c1c8c55
ikefix <omekeikechukwu877@gmail.com>
2026-02-03 15:16:14 +0100

    crsf1

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,19 +9,22 @@ class ContactController extends Controller
 {
     public function send(Request $request)
     {
+
         $data = $request->validate([
             'name' => 'required|string',
             'email' => 'required|email',
+            'subject' => 'required|string',
             'message' => 'required|string',
         ]);
 
         Mail::raw(
-            "Name: {$data['name']}\nEmail: {$data['email']}\nMessage: {$data['message']}",
-            function ($mail) {
+            "Name: {$data['name']}\nEmail: {$data['email']}\nSubject: {$data['subject']}\n\n{$data['message']}",
+            function ($mail) use ($data) {
                 $mail->to('support@bloommonie.com')
-                     ->subject('New Form Submission');
+                    ->subject($data['subject']);
             }
         );
+
 
         return back()->with('success', 'Form submitted successfully');
     }
